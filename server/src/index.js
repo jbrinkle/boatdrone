a772f669b38f9e5ab9b129f7ae0975f6c6e34d2f
Jason Brinkle <jason.brinkle@observepoint.com>
2020-11-24 23:29:27 -0700

    adding basic web sockets in server

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,26 @@
 const config = require('./config');
 const express = require('express');
+const ws = require('ws');
 
 const app = express();
+const wss = new ws.Server({ noServer: true });
 
 app.get('/status', (req, res) => {
     res.send('OK');
 });
 
-app.listen(config.port);
+wss.on('connection', socket => {
+    socket.on('message', message => {
+        console.log(`Received from browser: ${message}`);
+        socket.send('ACK');
+    });
+});
+
+const httpServer = app.listen(config.port);
+httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(
+        request,
+        socket,
+        head,
+        (socket) => wss.emit('connection', socket, request)); 
+});
